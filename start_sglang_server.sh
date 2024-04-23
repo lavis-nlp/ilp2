0e07734b96001297ebc1bd54db5afb5b7d8609ed
Lukas W <lukas.walker@student.hs-rm.de>
2024-04-23 10:12:15 +0200

    Improved start script

--- start_sglang_server.sh
+++ start_sglang_server.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-python3 -m sglang.launch_server --model-path llama-hf/ --port 30000 --random-seed 1
+
+# Überprüfen, ob Modellpfad übergeben wurde
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <model-path>"
+	exit 1
+fi
+
+MODEL_PATH=$1
+
+if ! [ -d "$MODEL_PATH" ]; then
+	echo "Model path doesnt exist!"
+	exit 1
+fi
+
+python3 -m sglang.launch_server --model-path "$MODEL_PATH" --port 30000
