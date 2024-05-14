11c0465a4fae7325d1ac29e72d26bb41b05732ef
Lukas W <lukas.walker@student.hs-rm.de>
2024-05-14 13:32:18 +0200

    Auto-Run

--- auto-test.sh
+++ auto-test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <model-path> <tensor-parallel-size>"
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 <model_path> <tensor_parallel_size> <system_prompt_path> <question_template_path>"
     exit 1
 fi
 
-model-path=$1
-tensor-parallel-size=$2
+model_path=$1
+tensor_parallel_size=$2
+system_prompt_path=$3
+question_template_path=$4
 
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 results_folder="Results-$timestamp"
@@ -16,6 +18,5 @@ datasets=("irt2-cde-tiny" "irt2-cde-small" "irt2-cde-medium" "irt2-cde-large")
 
 for dataset in "${datasets[@]}"; do
     output_file="$results_folder/$dataset.txt"
-    python_script.py "$dataset" "$model-path" "$tensor-parallel-size" | tee "$output_file"
+    python run_test.py "$dataset" "$model_path" "$tensor_parallel_size" "$system_prompt_path" "$question_template_path" | tee "$output_file"
 done
-
---
+++ run_test.py
@@ -0,0 +1,20 @@
+import sys
+from irt2_llm_prompter import model_prompter, test_logic, run_config
+
+# Überprüfe, ob genügend Argumente übergeben wurden
+if len(sys.argv) != 6:
+    print("Usage: python_script.py <model_path> <tensor_parallel_size> <system_prompt_path> <question_template_path> <dataset>")
+    sys.exit(1)
+
+# Extrahiere die Argumente aus der Befehlszeile
+dataset = sys.argv[1]
+model_path = sys.argv[2]
+tensor_parallel_size = int(sys.argv[3])
+system_prompt_path = sys.argv[4]
+question_template_path = sys.argv[5]
+
+# Konfiguration erstellen
+config = run_config.RunConfig.from_paths(question_template_path, system_prompt_path, dataset, model_path, tensor_parallel_size)
+
+# Testlogik ausführen
+test_logic.run_on_test(config)
