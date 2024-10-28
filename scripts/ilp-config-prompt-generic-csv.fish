b95ed2ce8020af1e810bfcf6c8cdb1048d11d491
Lukas <lukas.walker@student.hs-rm.de>
2024-10-28 10:49:25 +0100

    new scripts

--- scripts/ilp-config-prompt-generic-csv.fish
+++ scripts/ilp-config-prompt-generic-csv.fish
@@ -1,4 +1,4 @@
 set prompts \
-    --system-prompt conf/prompts/system/sysp-to-csv-v7.yaml \
+    --system-prompt conf/prompts/system/sysp-to-csv-v4.1.yaml \
     --prompt-template conf/prompts/template/template-generic-v14.txt \
     --question-template conf/prompts/question/prompt-templates-generic-v5.yaml
