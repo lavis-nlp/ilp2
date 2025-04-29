2c100ec03ca3787fb5839d4734abfdb638f24b10
Felix Hamann <felix@hamann.xyz>
2025-04-29 11:57:40 +0200

    added script-ilp2/ starter scripts

---
+++ scripts-ilp2/exp-starter-default.fish
@@ -0,0 +1,23 @@
+#!/usr/bin/env fish
+
+set root (dirname (status -f))
+
+. $root/config-env.fish
+. $root/config-dataset-irt.fish
+. $root/config-model-llama.fish
+
+set split validation
+set mode default
+
+set prompt_template conf/prompts/template/template-ripe-generic-v3.txt
+set system_prompt conf/prompts/system/sysp-to-csv-v4.1.yaml
+set question_template conf/prompts/question/prompt-templates-generic-v6.yaml
+
+set vertex_name --include-vertex-name
+set n_cands 0 # 5 10 15 20
+set mentions_per_candidate 10
+
+set output_suffix $split
+
+. $root/run.fish
+run_experiments $argv
---
+++ scripts-ilp2/exp-starter-frr.fish
@@ -0,0 +1,21 @@
+#!/usr/bin/env fish
+
+set root (dirname (status -f))
+
+. $root/config-env.fish
+. $root/config-dataset-irt.fish
+. $root/config-model-llama.fish
+
+set split validation
+set mode full-re-ranking
+
+set prompt_template conf/prompts/template/template-mode-3-v6.txt
+set system_prompt conf/prompts/system/sysp-mode-3-to-csv-v1.yaml
+set question_template conf/prompts/question/prompt-templates-generic-v6.yaml
+
+set vertex_name --include-vertex-name
+set n_cands 8
+set mentions_per_candidate 10
+
+. $root/run.fish
+run_experiments $argv
